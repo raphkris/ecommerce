8c2283ac254ef2833db77fa91d511df095044e84
Raphael Mangubat <raphaelmangubat@gmail.com>
2019-11-27 15:56:25 -0600

    saving changes. working on customer my account page.

--- customer/functions/functions.php
+++ customer/functions/functions.php
@@ -1,31 +1,31 @@
-<?php 
+<?php
 
-
+// After uploading to online server, change this connection accordingly
 $con = mysqli_connect("localhost","root","","ecommerce");
 
 if (mysqli_connect_errno())
 {
-	echo "The Connection was not established: " . mysqli_connect_error();
+  	echo "The Connection was not established: " . mysqli_connect_error();
 }
 
 // getting the user IP address
 function getIp()
 {
-    $ip = $_SERVER['REMOTE_ADDR'];
- 
+	$ip = $_SERVER['REMOTE_ADDR'];
+
 	if (!empty($_SERVER['HTTP_CLIENT_IP']))
 	{
 		$ip = $_SERVER['HTTP_CLIENT_IP'];
-		
+
 	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
 	{
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
- 
+		$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	}
+
 	return $ip;
-	
+
 }
-  
+
 //creating the shopping cart
 function cart()
 {
@@ -42,7 +42,7 @@ function cart()
 			echo "";
 
 		} else {
-		
+
 			$insert_pro = "insert into cart (p_id,ip_add) values ('$pro_id','$ip')";
 			$run_pro = mysqli_query($con, $insert_pro); 
 			echo "<script>window.open('index.php','_self')</script>";
@@ -64,15 +64,15 @@ function total_items()
 		$count_items = mysqli_num_rows($run_items);
 
 	} else {
-		
+
 		global $con; 
 		$ip = getIp(); 
 		$get_items = "select * from cart where ip_add='$ip'";
 		$run_items = mysqli_query($con, $get_items); 
 		$count_items = mysqli_num_rows($run_items);
-		
+
 	}
-	
+
 	echo $count_items;
 
 }
@@ -85,13 +85,13 @@ function total_price()
 	$ip = getIp(); 
 	$sel_price = "select * from cart where ip_add='$ip'";
 	$run_price = mysqli_query($con, $sel_price); 
-		
+	
 	while($p_price=mysqli_fetch_array($run_price))
 	{
 		$pro_id = $p_price['p_id']; 
 		$pro_price = "select * from products where product_id='$pro_id'";
 		$run_pro_price = mysqli_query($con,$pro_price); 
-			
+		
 		while ($pp_price = mysqli_fetch_array($run_pro_price))
 		{
 			$product_price = array($pp_price['product_price']);
@@ -99,9 +99,9 @@ function total_price()
 			$total +=$values;
 		}
 	}
-		
-	echo "$" . $total;
 	
+	echo "$" . $total;
+
 }
 
 //getting the categories
@@ -115,8 +115,13 @@ function getCats()
 	{
 		$cat_id = $row_cats['cat_id']; 
 		$cat_title = $row_cats['cat_title'];
-	
-		echo "<li><a href='index.php?cat=$cat_id'>$cat_title</a></li>";
+
+		// echo "<li><a href='index.php?cat=$cat_id'>$cat_title</a></li>";
+
+		// echo "<a class='py-2 d-none d-md-inline-block' href='index.php?cat=$cat_id' onclick='testDestroy();'>$cat_title</a>";
+
+		echo "<a class='py-2 d-none d-md-inline-block' href='index.php?cat=$cat_id'>$cat_title</a>";
+
 	}
 
 }
@@ -132,8 +137,10 @@ function getBrands()
 	{
 		$brand_id = $row_brands['brand_id']; 
 		$brand_title = $row_brands['brand_title'];
+	
+		// echo "<li><a href='index.php?brand=$brand_id'>$brand_title</a></li>";
 
-		echo "<li><a href='index.php?brand=$brand_id'>$brand_title</a></li>";
+		echo "<a class='dropdown-item' href='index.php?brand=$brand_id'>$brand_title</a>";
 	}
 
 }
@@ -147,7 +154,7 @@ function getPro()
 			global $con; 
 			$get_pro = "select * from products order by RAND() LIMIT 0,6";
 			$run_pro = mysqli_query($con, $get_pro); 
-	
+			
 			while($row_pro=mysqli_fetch_array($run_pro))
 			{
 				$pro_id = $row_pro['product_id'];
@@ -177,35 +184,75 @@ function getCatPro()
 	{
 		$cat_id = $_GET['cat'];
 		global $con; 
-		$get_cat_pro = "select * from products where product_cat='$cat_id'";
+
+		$get_cat_pro = "
+		select *, brand_title
+		from products
+			left join brands
+				on products.product_brand = brands.brand_id
+		where product_cat='$cat_id'
+		";
+
 		$run_cat_pro = mysqli_query($con, $get_cat_pro); 
 		$count_cats = mysqli_num_rows($run_cat_pro);
-	
+
+		echo "
+			<script>
+				$(function() {
+					$('.body-content').remove();
+				});
+			</script>
+		";
+
+		echo "
+			<div class='product-container'>
+				<div class='row pt-5 mt-5 mt-md-3 mt-lg-5'>
+		";
+		
 		if($count_cats==0)
 		{
-			echo "<h2 style='padding:20px;'>No products where found in this category!</h2>";
-		}
-		
-		while($row_cat_pro=mysqli_fetch_array($run_cat_pro))
-		{
-			$pro_id = $row_cat_pro['product_id'];
-			$pro_cat = $row_cat_pro['product_cat'];
-			$pro_brand = $row_cat_pro['product_brand'];
-			$pro_title = $row_cat_pro['product_title'];
-			$pro_price = $row_cat_pro['product_price'];
-			$pro_image = $row_cat_pro['product_image'];
 			echo "
-				<div id='single_product'>
-					<h3>$pro_title</h3>
-					<img src='admin_area/product_images/$pro_image' width='180' height='180' />
-					<p><b> $ $pro_price </b></p>
-					<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
-					<a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</button></a>
+				<div class='col-md-4 pb-5'>
+					<h2 class='font-weight-light text-muted'>Nothing to see here . . .</h2>
 				</div>
 			";
 		}
-	}
 
+		while($row_cat_pro=mysqli_fetch_array($run_cat_pro))
+		{
+			$pro_id = $row_cat_pro['product_id'];
+			$pro_brand_title = $row_cat_pro['brand_title'];
+			$pro_title = $row_cat_pro['product_title'];
+			$pro_price = $row_cat_pro['product_price'];
+			$pro_image = $row_cat_pro['product_image'];
+
+			echo "
+				<div class='col-md-4 pb-5'>
+					<div class='card-mb-4 '>
+						<div width='100%' height='225'>
+							<a href='details.php?pro_id=$pro_id'>
+								<img class='bd-placeholder-img card-img-top' src='admin_area/product_images/$pro_image' alt='' width='100%' height='100%'>
+							</a>
+						</div>
+						<div class='card-body pt-5 pb-0 px-0'>
+							<div class='d-flex justify-content-between align-items-center'>
+								<a href='details.php?pro_id=$pro_id' class='product-link'>
+									<small class='card-text text-muted'>$pro_brand_title $pro_title</small>
+								</a>
+								<small class='card-text'>$$pro_price</small>
+							</div>
+						</div>
+					</div>
+				</div>
+			";
+
+		}
+
+		echo "
+				</div>
+			</div>
+		";
+	}
 }
 
 function getBrandPro()
@@ -214,34 +261,72 @@ function getBrandPro()
 	{
 		$brand_id = $_GET['brand'];
 		global $con; 
-		$get_brand_pro = "select * from products where product_brand='$brand_id'";
+
+		$get_brand_pro = "
+		select *, brand_title
+		from products
+			left join brands
+				on products.product_brand = brands.brand_id
+		where product_brand='$brand_id'
+		";
+
 		$run_brand_pro = mysqli_query($con, $get_brand_pro); 
 		$count_brands = mysqli_num_rows($run_brand_pro);
-		
-		if($count_brands==0)
-		{
-			echo "<h2 style='padding:20px;'>No products where found associated with this brand!!</h2>";
+
+		echo "
+			<script>
+				$(function() {
+					$('.body-content').remove();
+				});
+			</script>
+		";
+
+		echo "
+			<div class='product-container'>
+				<div class='row pt-5 mt-5 mt-md-3 mt-lg-5'>
+		";
+
+		if ($count_brands == 0) {
+			echo "
+				<div class='col-md-4 pb-5'>
+					<h2 class='font-weight-light text-muted'>Nothing to see here . . .</h2>
+				</div>
+			";
 		}
 		
 		while($row_brand_pro=mysqli_fetch_array($run_brand_pro))
 		{
 			$pro_id = $row_brand_pro['product_id'];
-			$pro_cat = $row_brand_pro['product_cat'];
-			$pro_brand = $row_brand_pro['product_brand'];
+			$pro_brand_title = $row_brand_pro['brand_title'];
 			$pro_title = $row_brand_pro['product_title'];
 			$pro_price = $row_brand_pro['product_price'];
 			$pro_image = $row_brand_pro['product_image'];
+
 			echo "
-				<div id='single_product'>
-					<h3>$pro_title</h3>
-					<img src='admin_area/product_images/$pro_image' width='180' height='180' />
-					<p><b> $ $pro_price </b></p>
-					<a href='details.php?pro_id=$pro_id' style='float:left;'>Details</a>
-					<a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</button></a>
+				<div class='col-md-4 pb-5'>
+					<div class='card-mb-4 '>
+						<div width='100%' height='225'>
+							<a href='details.php?pro_id=$pro_id'>
+								<img class='bd-placeholder-img card-img-top' src='admin_area/product_images/$pro_image' alt='' width='100%' height='100%'>
+							</a>
+						</div>
+						<div class='card-body pt-5 pb-0 px-0'>
+							<div class='d-flex justify-content-between align-items-center'>
+								<a href='details.php?pro_id=$pro_id' class='product-link'>
+									<small class='card-text text-muted'>$pro_brand_title $pro_title</small>
+								</a>
+								<small class='card-text'>$$pro_price</small>
+							</div>
+						</div>
+					</div>
 				</div>
 			";
 		}
+
+		echo "
+				</div>
+			</div>
+		";
+
 	}
 }
-
-?>
--- customer/my_account.php
+++ customer/my_account.php
@@ -1,173 +1,297 @@
-<?php 
+<?php
 session_start();
 include("functions/functions.php");
 ?>
 
+<?php
+$user = $_SESSION['customer_email'];
+$get_img = "select * from customers where customer_email='$user'";
+$run_img = mysqli_query($con, $get_img);
+$row_img = mysqli_fetch_array($run_img);
+$c_image = $row_img['customer_image'];
+$c_name = $row_img['customer_name'];
+// echo "<p style='text-align:center;'><img src='customer_images/$c_image' width='150' height='150'/></p>";
+?>
+
+<?php
+$total = 0;
+global $con;
+$ip = getIp();
+$sel_price = "select * from cart where ip_add='$ip'";
+$run_price = mysqli_query($con, $sel_price);
+
+while ($p_price = mysqli_fetch_array($run_price)) {
+	$pro_id = $p_price['p_id'];
+	$pro_price = "select *, brand_title from products left join brands on products.product_brand = brands.brand_id where product_id='$pro_id'";
+	$run_pro_price = mysqli_query($con, $pro_price);
+
+	while ($pp_price = mysqli_fetch_array($run_pro_price)) {
+		$product_price = array($pp_price['product_price']);
+		$product_title = $pp_price['product_title'];
+		$product_image = $pp_price['product_image'];
+		$single_price = $pp_price['product_price'];
+		$values = array_sum($product_price);
+		$total += $values;
+	}
+}
+?>
+
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
 
 <head>
-	<title>My Online Shop</title>
-	<link rel="stylesheet" href="styles/style.css" media="all" /> 
-</head>
-	
-<body>
-	
-	<!--Main Container starts here-->
-	<div class="main_wrapper">
-	
-		<!--Header starts here-->
-		<div class="header_wrapper">
-			<a href="../index.php"><img id="logo" src="images/logo.gif" /> </a>
-			<img id="banner" src="images/ad_banner.gif" />
-		</div>
-		<!--Header ends here-->
-		
-		<!--Navigation Bar starts-->
-		<div class="menubar">
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-			<ul id="menu">
-				<li>
-					<a href="ecommerce/index.php">Home</a>
-				</li>
-				<li>
-					<a href="../all_products.php">All Products</a>
-				</li>
-				<li>
-					<a href="customer/my_account.php">My Account</a>
-				</li>
-				<li>
-					<a href="#">Sign Up</a>
-				</li>
-				<li>
-					<a href="cart.php">Shopping Cart</a>
-				</li>
-				<li>
-					<a href="#">Contact Us</a>
-				</li>
-			</ul>
-			
-			<div id="form">
-				<form method="get" action="results.php" enctype="multipart/form-data">
-					<input type="text" name="user_query" placeholder="Search a Product"/> 
-					<input type="submit" name="search" value="Search" />
-				</form>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+
+<body>
+
+	<nav class="site-header sticky-top py-1" id="nav-contain">
+		<div class="container d-flex flex-column flex-md-row justify-content-around" id="nav-content">
+			<a class="py-2" href="index.php">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24" focusable="false">
+					<title>Product</title>
+					<circle cx="12" cy="12" r="10" />
+					<path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94" />
+				</svg>
+			</a>
+			<?php getCats(); ?>
+			<div class="dropdown">
+				<a class="py-2 d-none d-md-inline-block" data-toggle="dropdown" href="">Shop</a>
+				<div class="dropdown-menu">
+					<a class="dropdown-item" href="all_products.php">All Products</a>
+					<div class="dropdown-divider"></div>
+					<?php getBrands(); ?>
+				</div>
 			</div>
-			
+			<a class="py-2 d-none d-md-inline-block" href="#" data-toggle="modal" data-target="#search-modal">Search</a>
+			<div class="dropdown">
+				<a class="py-2 d-none d-md-inline-block" data-toggle="dropdown" href="">Bag</a>
+				<div class="dropdown-menu">
+					<a class="dropdown-item" href="cart.php">Bag</a>
+					<div class="dropdown-divider"></div>
+					<?php
+					if (isset($_SESSION['customer_email'])) {
+						echo "<a class='dropdown-item' href='customer/my_account.php'>Account</a>";
+						echo "<a class='dropdown-item' href='logout.php'>Log out</a>";
+					} else {
+						echo "<a class='dropdown-item' href='customer/my_account.php'>Account</a>";
+						echo "<a class='dropdown-item' href='#'>Log in</a>";
+					}
+					?>
+				</div>
+			</div>
 		</div>
-		<!--Navigation Bar ends-->
-	
-		<!--Content wrapper starts-->
-		<div class="content_wrapper">
-		
-			<div id="sidebar">
-			
-				<div id="sidebar_title">My Account:</div>
-				
-				<ul id="cats">
-					<?php 
-						$user = $_SESSION['customer_email'];
-						$get_img = "select * from customers where customer_email='$user'";
-						$run_img = mysqli_query($con, $get_img); 
-						$row_img = mysqli_fetch_array($run_img); 
-						$c_image = $row_img['customer_image'];
-						$c_name = $row_img['customer_name'];
-						echo "<p style='text-align:center;'><img src='customer_images/$c_image' width='150' height='150'/></p>";
+	</nav>
+
+	<div class="modal" tabindex="-1" role="dialog" id="search-modal">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-body">
+					<form method="get" action="results.php" enctype="multipart/form-data">
+						<div class="row justify-content-center">
+							<div class="input-group col-md-12" id="form">
+								<input class="form-control py-2 border-right-0 border" type="text" placeholder="Search" name="user_query" />
+								<span class="input-group-append">
+									<button class="btn btn-outline-secondary border-left-0 border" type="submit" name="search">
+										<i class="fa fa-search"></i>
+									</button>
+								</span>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="body-content">
+
+		<div class="w100 bg-light px-5 pb-4">
+			<div class='container'>
+				<div class="row px-5 pt-4">
+					<div class="col-6">
+						<h4 class="text-left">Account</h4>
+					</div>
+					<div class="col-6">
+						<p class="text-right align-text-bottom"><a class="" href="logout.php"><small class="">Sign Out</small></a></p>
+					</div>
+				</div>
+				<hr />
+				<div class="row px-5">
+					<div class="col-12">
+						<h1 class="text-left pt-4">Hi, <?php echo $c_name ?>.</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="w100 px-5 pb-4">
+			<div class="container">
+				<div class="row px-5 pt-5">
+					<div class="col-12">
+						<h1 class="text-left pt-4">Your Bag</h1>
+					</div>
+				</div>
+				<div class="row px-5">
+
+					<?php
+					$total = 0;
+					global $con;
+					$ip = getIp();
+					$sel_price = "select * from cart where ip_add='$ip'";
+					$run_price = mysqli_query($con, $sel_price);
+
+					while ($p_price = mysqli_fetch_array($run_price)) {
+						$pro_id = $p_price['p_id'];
+						$pro_price = "select *, brand_title from products left join brands on products.product_brand = brands.brand_id where product_id='$pro_id'";
+						$run_pro_price = mysqli_query($con, $pro_price);
+
+						while ($pp_price = mysqli_fetch_array($run_pro_price)) {
+							$product_price = array($pp_price['product_price']);
+							$product_title = $pp_price['product_title'];
+							$pro_brand_title = $pp_price['brand_title'];
+							$product_image = $pp_price['product_image'];
+							$single_price = $pp_price['product_price'];
+							$values = array_sum($product_price);
+							$total += $values;
+
+							echo "
+								<div class='col-md-4 pb-5 bg-light'>
+									<div class='card-mb-4 '>
+										<div width='100%' height='225'>
+											<a href='details.php?pro_id=$pro_id'>
+												<img class='bd-placeholder-img card-img-top' src='../admin_area/product_images/$product_image' alt='' width='100%' height='100%'>
+											</a>
+										</div>
+										<div class='card-body pt-5 pb-0 px-0'>
+											<div class='d-flex justify-content-between align-items-center'>
+												<a href='details.php?pro_id=$pro_id' class='product-link'>
+													<small class='card-text text-muted'>$$pro_brand_title $product_title</small>
+												</a>
+												<small class='card-text'>$123</small>
+											</div>
+										</div>
+									</div>
+								</div>
+							";
+						}
+					}
 					?>
 
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<footer class="pt-5 pt-md-5 container" id="foot-contain">
+		<div class="pt-5 mt-3 pt-md-5 pl-md-5 row justify-content-around">
+			<div class="pl-md-4 col-2 col-md">
+				<h7>first header</h7>
+				<ul class="list-unstyled text-small">
 					<li>
-						<a href="my_account.php?my_orders">My Orders</a>
+						<a class="text-muted" href="#">first list item</a>
 					</li>
 					<li>
-						<a href="my_account.php?edit_account">Edit Account</a>
+						<a class="text-muted" href="#">second list item</a>
 					</li>
 					<li>
-						<a href="my_account.php?change_pass">Change Password</a>
+						<a class="text-muted" href="#">third list item</a>
 					</li>
 					<li>
-						<a href="my_account.php?delete_account">Delete Account</a>
+						<a class="text-muted" href="#">fourth list item</a>
 					</li>
 					<li>
-						<a href="logout.php">Logout</a>
+						<a class="text-muted" href="#">fifth list item</a>
 					</li>
 				</ul>
-				
 			</div>
-		
-			<div id="content_area">
-			
-				<?php cart(); ?>
-				
-				<div id="shopping_cart"> 
-					<span style="float:right; font-size:17px; padding:5px; line-height:40px;">
-					
-						<?php 
-							if(isset($_SESSION['customer_email']))
-							{
-								echo "<b>Welcome:</b>" . $_SESSION['customer_email'] ;
-							}
-						?>
-						
-						<?php 
-							if(!isset($_SESSION['customer_email']))
-							{
-								echo "<a href='checkout.php' style='color:orange;'>Login</a>";
-							} else {
-								echo "<a href='logout.php' style='color:orange;'>Logout</a>";
-							}
-						?>
-					
-					</span>
-				</div>
-			
-				<div id="products_box">
-				
-					<?php 
-						if(!isset($_GET['my_orders']))
-						{
-							if(!isset($_GET['edit_account']))
-							{
-								if(!isset($_GET['change_pass']))
-								{
-									if(!isset($_GET['delete_account']))
-									{
-										echo "
-											<h2 style='padding:20px;'>Welcome:  $c_name </h2>
-											<b>You can see your orders progress by clicking this <a href='my_account.php?my_orders'>link</a></b>
-										";
-									}
-								}
-							}
-						}
-					?>
-					<?php 
-						if(isset($_GET['edit_account']))
-						{
-							include("edit_account.php");
-						}
-						if(isset($_GET['change_pass']))
-						{
-							include("change_pass.php");
-						}
-						if(isset($_GET['delete_account']))
-						{
-							include("delete_account.php");
-						}
-					?>
-				
-				</div>
-			
+			<div class="pl-md-4 col-2 col-md">
+				<h7>second header</h7>
+				<ul class="list-unstyled text-small">
+					<li>
+						<a class="text-muted" href="#">first list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">second list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">third list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">fourth list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">fifth list item</a>
+					</li>
+				</ul>
 			</div>
+			<div class="pl-md-4 col-2 col-md">
+				<h7>third header</h7>
+				<ul class="list-unstyled text-small">
+					<li>
+						<a class="text-muted" href="#">first list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">second list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">third list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">fourth list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">fifth list item</a>
+					</li>
+				</ul>
+			</div>
+			<div class="pl-md-4 col-2 col-md">
+				<h7>fourth header</h7>
+				<ul class="list-unstyled text-small">
+					<li>
+						<a class="text-muted" href="#">first list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">second list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">third list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">fourth list item</a>
+					</li>
+					<li>
+						<a class="text-muted" href="#">fifth list item</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="pt-3 pt-md-5 row text-center">
+			<div class="col-12 col-md" id="foot-info">
+				<p class="text-muted">foot info placeholder text</p>
+			</div>
+		</div>
+	</footer>
 
-		</div>
-		<!--Content wrapper ends-->
-		
-		<div id="footer">
-			<h2 style="text-align:center; padding-top:30px;">&copy; </h2>
-		</div>
-	
-	</div> 
-	<!--Main Container ends here-->
+	<!-- Optional JavaScript -->
+	<script src="js/script.js"></script>
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
